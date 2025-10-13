a21e4ce11b6339b75031d29815ee1a93d7306897
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2025-10-13 20:19:23 -0300

    Django-Extensions

--- projetoFeriado/feriados/feriados/settings.py
+++ projetoFeriado/feriados/feriados/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party
+    'django_extensions',
     # My Apps
     'core.apps.CoreConfig',
 ]
