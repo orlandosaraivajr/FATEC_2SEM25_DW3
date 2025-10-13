68b76e755b350e2cfb995cf88d8824a32c2e9223
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2025-10-13 20:08:34 -0300

    Django Admin

--- projetoFeriado/feriados/core/admin.py
+++ projetoFeriado/feriados/core/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from core.models import FeriadoModel
+from datetime import datetime
 
-admin.site.register(FeriadoModel)
+class FeriadoModelAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'dia', 'mes', 'modificado_em', 'registro_no_mes')
+    list_filter = ('mes',)
+    search_fields = ('nome',)
+    ordering = ('mes', 'dia')
+    readonly_fields = ('modificado_em',)
+    
+    def registro_no_mes(self, obj):
+        hoje = datetime.now()
+        return obj.mes == hoje.month
+    
+    registro_no_mes.short_description = 'Feriado neste mês?'
+    registro_no_mes.boolean = True
+
+admin.site.register(FeriadoModel, FeriadoModelAdmin)
